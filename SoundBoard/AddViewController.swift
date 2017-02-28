d21796322cd2f98ce1d697ffc4abebe5429f9314
KR DEVARAJAN NAIR <KrishPinky@KRs-MacBook-Pro.local>
2017-02-28 19:12:48 +0200

    play and record completed

--- SoundBoard/AddViewController.swift
+++ SoundBoard/AddViewController.swift
@@ -19,11 +19,16 @@ class AddViewController: UIViewController {
     @IBOutlet weak var lblAdd: UIButton!
     
     var audioRecorder:AVAudioRecorder?
+    var audioPlayer=AVAudioPlayer()
+    var audioUrl:URL?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        setUpRecorder()
+    lblPlay.isEnabled=false
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,18 +36,56 @@ class AddViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    function setUpRecorder(){
-    
-    
+    func setUpRecorder(){
+        do{
+            // Activate AudioSession
+            let session=AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            //Create audio URL
+            let basePath:String=NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents=[basePath,"audio1.m4a"]
+            audioUrl=NSURL.fileURL(withPathComponents: pathComponents)
+            txtTitle.text=String(describing: audioUrl)
+            //Add settings to dictionary
+            var settings:[String:Any]=[:]
+            settings[AVFormatIDKey]=Int(kAudioFormatMPEG4AAC)
+            settings[AVSampleRateKey]=44100.0
+            settings[AVNumberOfChannelsKey]=2
+            
+            audioRecorder=try AVAudioRecorder(url: audioUrl!, settings: settings)
+            audioRecorder!.prepareToRecord()
+        }
+            
+        catch let error as NSError{
+            print(error)
+        }
+        
+        
     }
     
     @IBAction func btnRecord(_ sender: AnyObject) {
-        
+        if audioRecorder!.isRecording{
+            audioRecorder?.stop()
+            lblRecord.setTitle("Record", for: .normal)
+            lblPlay.isEnabled=true
+        }
+        else{
+            audioRecorder?.record()
+            lblRecord.setTitle("Stop", for: .normal)
+        }
     }
     
     
     @IBAction func btnPlay(_ sender: AnyObject) {
-        
+        do{
+            try audioPlayer = AVAudioPlayer(contentsOf: audioUrl!)
+            audioPlayer.play()
+        }
+        catch {
+            print(error)
+        }
     }
     
     @IBAction func btnAdd(_ sender: AnyObject) {
